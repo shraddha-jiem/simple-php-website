e909c97a07bfda5d041e95515b0d45017cd9c949
shraddha-jiem <83442323+shraddha-jiem@users.noreply.github.com>
2023-03-27 16:55:31 +0530

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,5 +2,5 @@
 
 # Install PHP and dependencies
 amazon-linux-extras enable php8.0
-yum install php php-common php-pear -y
-yum install php-{cgi,curl,mbstring,gd,mysqlnd,gettext,json,xml,fpm,intl,zip}
+yum clean metadata
+yum install php-{pear,cgi,pdo,common,curl,mbstring,gd,mysqlnd,gettext,bcmath,json,xml,fpm,intl,zip} -y
