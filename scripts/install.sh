12e2198c19460111dab4dd4f9433334403bd9f0a
shraddha-jiem <83442323+shraddha-jiem@users.noreply.github.com>
2023-03-27 15:58:58 +0530

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Install Apache/httpd and start it
-yum install -y httpd httpd-tools mod_ssl
-systemctl start httpd  
-
 # Install PHP and dependencies
 amazon-linux-extras enable php8.0
 yum install php php-common php-pear -y
