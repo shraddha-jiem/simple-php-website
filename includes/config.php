c5623259699f4e6258f0b32ba1afc16c9bf73d00
sharey <sharey@gmail>
2025-08-06 09:44:30 +0530

    Changes added

--- includes/config.php
+++ includes/config.php
@@ -8,7 +8,7 @@
 function config($key = '')
 {
     $config = [
-        'name' => 'Simple PHP Website (Dev) - V1.7',
+        'name' => 'Simple PHP Website (Dev) - V1.8',
         'site_url' => '',
         'pretty_uri' => false,
         'nav_menu' => [
